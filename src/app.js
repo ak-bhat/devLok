e7f93c02ccf29d15f1086f94cf73e08189538814
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2025-07-21 15:59:42 +0530

    Exploring http methods with routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require('express')
 
 const app = express()
 
+app.use("/test//hi", (req, res)=>{
+    res.send("Test page 2")
+})
+
+app.get("/user", (req, res)=>{
+    res.send({
+        name:"Ananthu",
+        age:25
+    })
+})
+
+app.post("/user", (req, res)=>{
+    res.send('User data uploaded successfully!')
+})
+
+app.delete("/user", (req, res)=>{
+    res.send('User data deleted successfully!')
+})
+
 app.use("/test", (req, res)=>{
     res.send("Test page")
 })
@@ -10,9 +29,14 @@ app.use("/profile",(req, res)=>{
     res.send("Welcome to profile page")
 })
 
-app.use((req, res)=>{
-    res.send("Hello from the Server")    
-})
+// app.use((req, res)=>{
+//     res.send("Hello from the Server")    
+// })
+
+// app.use("/",(req, res)=>{
+//     res.send("Hello from the Ananthu")    
+// })
+
 
 app.listen(7777,()=>{
     console.log("Server running at 7777")
